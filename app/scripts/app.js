83b9264007ef6c8544e5a2ef51915261dfbe78f1
Dane Petersen <thegreatsunra@gmail.com>
2014-06-20 07:42:26 -0700

    Update app.js with barcode directive code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+angular.module('myApp', [])
+  .controller('HomeController', function($scope) {
+    $scope.barcode = '9002236311037';
+  })
+  .directive('barcode', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        code: '='
+      },
+      compile: function (elem, attrs) {
+        var canvas = angular.element(
+          '<canvas width="300" height="200"></canvas>');
+        elem.append(canvas);
+        return function (scope, elem, attrs) {
+          scope.$watch('code', function(newValue) {
+            if (newValue) {
+              canvas.EAN13(newValue.toString());
+            }
+          });
+        };
+      }
+    };
+  });
